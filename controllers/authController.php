ab0b0650e8c6bfbeb999178e2b76abbb010437f2
OctoPK <armand@biguey.com>
2025-11-26 16:53:01 +0100

    add options for password hash

--- controllers/authController.php
+++ controllers/authController.php
@@ -1,10 +1,16 @@
 <?php
 
-/*Gère inscription/connexion/déconnexion des utilisateurs : 
+/*Gère inscription/connexion/déconnexion des utilisateurs :
 Il reçoit les données du formulaire de connexion ($_POST)
 Il vérifie si le mot de passe correspond au hash dans la BDD
 Il crée la $_SESSION si c'est bon ou renvoie une erreur si c'est faux
 */
+$options = [
+    'memory_cost' => 1 <<17,
+    'time_cost' => 4,
+    'threads' => 2,
+];
+
 
     class AuthController {
 
@@ -13,14 +19,14 @@ Il crée la $_SESSION si c'est bon ou renvoie une erreur si c'est faux
             require __DIR__ . '/../views/auth/register.php';
         }
 
-        public function register($username, $password, $email) { // Logique d'inscription utilisation de la base de donnee 
+        public function register($username, $password, $email) { // Logique d'inscription utilisation de la base de donnee
             // ici je recupere les donnees du formulaire que l'utilisateur a rempli
             $username = htmlspecialchars($_POST['username']);
             $email = filter_var($_POST['email']);
             $password = $_POST['password'];
             $confirm_password = $_POST['confirm_password'];
 
-            // je fais des verifications des données 
+            // je fais des verifications des données
 
             if (!$email || empty($username) || empty($password)) {
                 $_SESSION['message_erreur'] = "Tous les champs sont obligatoires";
@@ -35,9 +41,10 @@ Il crée la $_SESSION si c'est bon ou renvoie une erreur si c'est faux
             }
 
             // hachage du mot de passe
-            
+            $hash = password_hash($password, PASSWORD_ARGON2ID, $options);
+
         }
-        
+
         public function login($username, $password) {
             require __DIR__ . '/../views/auth/login.php';
         }
@@ -47,4 +54,4 @@ Il crée la $_SESSION si c'est bon ou renvoie une erreur si c'est faux
         }
 
     }
-?> 
+?>
