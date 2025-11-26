3793eb24fbc76c12fa44d4839fbdcb4a2e3517bd
hichem1914 <hichemchellal19@gmail.com>
2025-11-26 15:09:53 +0100

    ajout du dossier controller

---
+++ controllers/authController.php
@@ -0,0 +1,8 @@
+<?php
+
+/*Gère inscription/connexion/déconnexion des utilisateurs : 
+Il reçoit les données du formulaire de connexion ($_POST)
+Il vérifie si le mot de passe correspond au hash dans la BDD
+Il crée la $_SESSION si c'est bon ou renvoie une erreur si c'est faux
+*/
+?> 
---
+++ controllers/gameController.php
@@ -0,0 +1,15 @@
+<?php
+
+/* gere le gameplay, les chapitres, les choix des joueurs, etc 
+
+C'est lui qui définit les variables $chapitre par exemple dans la page chapitre.php, $hero, $links que tu utilises dans tes Vues.
+*/
+
+class gameController {
+    public function lectureChapitre($numeroChapitre) {
+             
+    }
+}
+
+
+?>
